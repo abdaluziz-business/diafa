d64fabe1e1224696e2e695bc62fb6d9c3ff90f64
abdulaziz alrehaili <abdaluziz.ma@gmail.com>
2026-03-04 06:51:13 +0300

    Add main App component with multiple sections

---
+++ Riyadh/App.tsx
@@ -0,0 +1,22 @@
+import HeroSection from "./components/HeroSection"
+import RoomsSection from "./components/RoomsSection"
+import GallerySection from "./components/GallerySection"
+import ServicesSection from "./components/ServicesSection"
+import TestimonialsSection from "./components/TestimonialsSection"
+import PartnersSection from "./components/PartnersSection"
+import ContactSection from "./components/ContactSection"
+
+export default function App() {
+
+  return (
+    <>
+      <HeroSection />
+      <RoomsSection />
+      <GallerySection />
+      <ServicesSection />
+      <TestimonialsSection />
+      <PartnersSection />
+      <ContactSection />
+    </>
+  )
+}
